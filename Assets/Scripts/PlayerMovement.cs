cd74d9613d04dac477929345f5fe878d98440be8
BubbsStep <bubbs7293@gmail.com>
2025-11-08 19:08:47 -0500

    Player Controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
 
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");  
+        float moveZ = Input.GetAxisRaw("Vertical");    
 
-        moveInput = new Vector2(moveX, moveY).normalized;
-    }
+        Vector3 move = new Vector3(moveX, 0f, moveZ) * speed * Time.deltaTime;
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+        transform.Translate(move, Space.World);
     }
 }
