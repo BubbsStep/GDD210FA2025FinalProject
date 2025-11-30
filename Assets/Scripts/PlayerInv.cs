5668ee1322f4a77f39e7293ad8720d3b818200ab
hjplourde <haydenjplourde@gmail.com>
2025-11-29 23:35:16 -0500

    Shop Update
    Close to full completion, just need to ask Prof about some coding because of some progression issues.

--- Assets/Scripts/PlayerInv.cs
+++ Assets/Scripts/PlayerInv.cs
@@ -5,4 +5,12 @@ public class PlayerInv : MonoBehaviour
 {
     static public List<string> Inventory = new List<string>();
 
+    static public int playerCurrency = 100000;
+    //Change after making a drop that's possible to pick up
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
 }
