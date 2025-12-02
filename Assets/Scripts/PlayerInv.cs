cfbe123d5dd3f1eb0c93802ecd93bb2ce7f3d96d
hjplourde <haydenjplourde@gmail.com>
2025-12-02 12:36:45 -0500

    Blake Shop

--- Assets/Scripts/PlayerInv.cs
+++ Assets/Scripts/PlayerInv.cs
@@ -5,8 +5,13 @@ public class PlayerInv : MonoBehaviour
 {
     static public List<string> Inventory = new List<string>();
 
-    static public int playerCurrency = 100000;
+    static public int playerCurrency;
     //Change after making a drop that's possible to pick up
+
+    
+    static public List<Blake.Item> Swords => Blake.Shop.swords;
+    [SerializeField] private List<Blake.Item> swords = new List<Blake.Item>();
+
 
     void Awake()
     {
